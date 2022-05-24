529b69e0b2f1010462c02f26c2bc0132b46a3fcf
clean_water <shmizuyuta213@icloud.com>
2022-05-24 14:02:27 +0900

    ロゴ追加

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -4,14 +4,13 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-
 const Navbar = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            App
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} >
+          <img src={`${process.env.PUBLIC_URL}/logo.png`} style={{ width:'10rem',backgroundColor:'white'}} alt="Logo" />
           </Typography>
           <Button color="inherit" href='/SignUp'>SignUp</Button>
           <Button color="inherit" href='/Login'>Login</Button>
